1c97daebc610eb2411acb8eafcd9d2c2a0b5782d
Sodiq Yusuff <ysodiqakanni@gmail.com>
2019-09-01 18:33:24 -0400

    Add project files.

---
+++ TickettingSystem.Api/Controllers/ClientsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TickettingSystem.Api.Controllers
+{
+    
+    /// <summary>
+    /// Endpoints to manage Staff
+    /// </summary>
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ClientsController : ControllerBase
+    { 
+        public async Task<IActionResult> CreateStaff()
+        { 
+
+            return Ok();
+        }
+    }
+}
---
+++ TickettingSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TickettingSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
