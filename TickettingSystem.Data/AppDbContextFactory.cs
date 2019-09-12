36359508398dc864c541dd526613b9bda9feb6e1
Temitope Alagbada <alagbadatemitope77@gmail.com>
2019-09-12 17:24:43 +0100

    add dbcontext factory

---
+++ TickettingSystem.Data/AppDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TickettingSystem.Data
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            var connection = @"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=TicketingDb;Data Source=.";
+            optionsBuilder.UseSqlServer(connection);
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
